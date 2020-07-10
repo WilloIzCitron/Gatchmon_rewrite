18141719aac511e243553542969b9f56e5d313cc
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2020-07-10 16:35:27 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,16 @@
+import discord
+import os
+import dotenv
+from discord.ext import commands
+from datetime import datetime
+timestamp = datetime.now()
 
+token = os.getenv('TOKEN')
+bot = commands.Bot(command_prefix='g!')
+client = discord.Client()
+
+@bot.event
+async def on_ready():
+  print('bot online')
+  
+bot.run(token)
